414ffa2fdd9079213d39244638259160b7ba7be3
Maciej Sadoś <s14051@pjwstk.edu.pl>
2018-01-13 19:46:21 +0100

    Cleaned and prepared solution to work

--- SzkolaWebApp/Global.asax.cs
+++ SzkolaWebApp/Global.asax.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace SzkolaWebApp
@@ -13,9 +8,7 @@ namespace SzkolaWebApp
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
