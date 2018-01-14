de48648e622ee73357601f23cdba94213c41611a
Maciej Sadoś <s14051@pjwstk.edu.pl>
2018-01-14 15:14:18 +0100

    Added google maps view in Contact

--- SzkolaWebApp/Scripts/google-map.js
+++ SzkolaWebApp/Scripts/google-map.js
@@ -1,14 +1,14 @@
 function initMap() {
-    var uluru = {lat: 52.171040, lng: 20.438797};
+    var szkola = {lat: 52.171040, lng: 20.438797};
 
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 17,
-        mapTypeId: 'satellite',
-        center: uluru
+        mapTypeId: 'hybrid',
+        center: szkola
     });
 
     var marker = new google.maps.Marker({
-        position: uluru,
+        position: szkola,
         map: map
     });
 }
