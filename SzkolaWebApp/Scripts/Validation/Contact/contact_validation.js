25593efd7779bb39f3232e7c64854d94e9f27ff9
Maciej Sadoś <s14051@pjwstk.edu.pl>
2018-01-26 14:58:15 +0100

    Added client-side validation to 'edit contact' page

---
+++ SzkolaWebApp/Scripts/Validation/Contact/contact_validation.js
@@ -0,0 +1,18 @@
+﻿var content = document.forms["contact-form"]["Content"].value;
+var errorBox = document.getElementById('contact-error-box');
+
+function validateContactForm() {
+    if (content == null || content.trim().length == 0) {
+        showContactError("Treść nie może być pusta");
+        return false;
+    }
+    if (content.length > 2000) {
+        showContactError("Treść nie może być dłuższa niż 2000 znaków");
+        return false;
+    }
+    return true;
+}
+
+function showContactError(error) {
+    errorBox.innerHTML = error;
+}
