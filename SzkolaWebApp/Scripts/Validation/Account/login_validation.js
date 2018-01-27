2face1f1eae68248e4725e7d77ae7a543683ad29
Maciej Sadoś <s14051@pjwstk.edu.pl>
2018-01-27 11:49:27 +0100

    Implemented client-side validation for AccountController's operations

--- SzkolaWebApp/Scripts/Validation/Account/login_validation.js
+++ SzkolaWebApp/Scripts/Validation/Account/login_validation.js
@@ -1,1 +1,54 @@
 ﻿document.getElementById('login-username').focus();
+
+
+var form = document.forms['login-form'];
+var usernameErrorMessage;
+var passwordErrorMessage;
+
+form.onsubmit = function () {
+    clearErrors();
+    var username = form['login-username'].value;
+    var password = form['login-password'].value;
+
+    return areCredentialsValid(username, password);
+};
+
+function areCredentialsValid(username, password) {
+    var validationResult = true;
+
+    if (isNullOrEmpty(username)) {
+        usernameErrorMessage = "Nazwa użytkownika nie może być pusta";
+        validationResult = false;
+    }
+    else if (username.length > 20) {
+        usernameErrorMessage = "Wprowadzona nazwa użytkownika jest za długa";
+        validationResult = false;
+    }
+
+    if (isNullOrEmpty(password) || password.length < 5) {
+        passwordErrorMessage = "Hasło musi składać się z przynajmniej 5 znaków";
+        validationResult = false;
+    }
+    else if (password.length > 4000) {
+        passwordErrorMessage = "Hasło nie może mieć więcej niż 4000 znaków";
+        validationResult = false;
+    }
+
+    showLoginErrors();
+    return validationResult;
+}
+
+function isNullOrEmpty(val) {
+    return val == null || val.trim().length === 0;
+}
+
+function clearErrors() {
+    document.getElementById('login-credentials-error').innerHTML = '';
+    usernameErrorMessage = '';
+    passwordErrorMessage = '';
+}
+
+function showLoginErrors() {
+    document.getElementById('login-username-error-message').innerHTML = usernameErrorMessage;
+    document.getElementById('login-password-error-message').innerHTML = passwordErrorMessage;
+}
--- SzkolaWebApp/Scripts/Validation/Account/registration_validation.js
+++ SzkolaWebApp/Scripts/Validation/Account/registration_validation.js
@@ -1,1 +1,54 @@
 ﻿document.getElementById('register-username').focus();
+
+
+var form = document.forms['register-form'];
+var usernameErrorMessage;
+var passwordErrorMessage;
+
+form.onsubmit = function () {
+    clearErrors();
+    var username = form['register-username'].value;
+    var password = form['register-password'].value;
+
+    return areCredentialsValid(username, password);
+};
+
+function areCredentialsValid(username, password) {
+    var validationResult = true;
+
+    if (isNullOrEmpty(username)) {
+        usernameErrorMessage = "Nazwa użytkownika nie może być pusta";
+        validationResult = false;
+    }
+    else if (username.length > 20) {
+        usernameErrorMessage = "Wprowadzona nazwa użytkownika jest za długa";
+        validationResult = false;
+    }
+
+    if (isNullOrEmpty(password) || password.length < 5) {
+        passwordErrorMessage = "Hasło musi składać się z przynajmniej 5 znaków";
+        validationResult = false;
+    }
+    else if (password.length > 4000) {
+        passwordErrorMessage = "Hasło nie może mieć więcej niż 4000 znaków";
+        validationResult = false;
+    }
+
+    showLoginErrors();
+    return validationResult;
+}
+
+function isNullOrEmpty(val) {
+    return val == null || val.trim().length === 0;
+}
+
+function clearErrors() {
+    document.getElementById('register-credentials-error').innerHTML = '';
+    usernameErrorMessage = '';
+    passwordErrorMessage = '';
+}
+
+function showLoginErrors() {
+    document.getElementById('register-username-error-message').innerHTML = usernameErrorMessage;
+    document.getElementById('register-password-error-message').innerHTML = passwordErrorMessage;
+}
